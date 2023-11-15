68153160661cc25a2f6bb6b1383d5f1c113239c0
tsaist1 <shengtsetsai@gmail.com>
2023-11-15 03:25:10 -0800

    +

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Date from '../components/date';
 import Head from 'next/head';
+import { useState } from 'react';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
@@ -17,9 +18,7 @@ export default function Home({ allPostsData }) {
                 <p>[Welcome to ST's website]</p>
             </section>
 
-            <section
-                className={`${utilStyles.headingMd}, ${utilStyles.padding1px}`}
-            >
+            <section className={`${utilStyles.headingMd}, ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
                     {allPostsData.map(({ id, date, title }) => (
@@ -62,68 +61,4 @@ function Profile() {
     if (error) return <div>failed to load</div>;
     if (!data) return <div>loading...</div>;
     return <div>hello {data.name}!</div>;
-}
-
-function About() {
-    return (
-        <embed
-            src="../public/pdfs/ShengTseTsai_Resume.pdf"
-            width="800px"
-            height="2100px"
-        />
-    );
-}
-
-function SocialMediaLinks() {
-    const [githubUrl, setGithubUrl] = useState('');
-    const [linkedinUrl, setLinkedinUrl] = useState('');
-
-    return (
-        <div>
-            <label>
-                GitHub:
-                <input
-                    type="text"
-                    value={githubUrl}
-                    onChange={(e) => setGithubUrl(e.target.value)}
-                />
-            </label>
-            <br />
-            <label>
-                LinkedIn:
-                <input
-                    type="text"
-                    value={linkedinUrl}
-                    onChange={(e) => setLinkedinUrl(e.target.value)}
-                />
-            </label>
-            <br />
-            <div>
-                {githubUrl && (
-                    <p>
-                        GitHub:{' '}
-                        <a
-                            href={githubUrl}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            {githubUrl}
-                        </a>
-                    </p>
-                )}
-                {linkedinUrl && (
-                    <p>
-                        LinkedIn:{' '}
-                        <a
-                            href={linkedinUrl}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            {linkedinUrl}
-                        </a>
-                    </p>
-                )}
-            </div>
-        </div>
-    );
 }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -18,9 +18,7 @@ export default function Post({ postData }) {
                 <div className={utilStyles.lightText}>
                     <Date dateString={postData.date} />
                 </div>
-                <div
-                    dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
-                />
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
             </article>
         </Layout>
     );
